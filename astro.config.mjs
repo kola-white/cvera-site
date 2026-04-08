591c6626cfeb061abc73fd5e7a05a5b6c4c864b9
kola-white <3113184+kola-white@users.noreply.github.com>
2026-04-08 11:29:15 -0700

    Astro cvera-site project setup and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://www.cvera.app',
+    vite: {
+    plugins: [tailwindcss()],
+  },
+});
